257fe94f64d4fb3f7dddf78510cb021ac5156ec5
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2024-04-11 16:29:04 +0300

    implemented drawDefaultUI function and mouseClick listener. drawDefaultUI:     draws blue T figure from the center of the given coords(default - center of our window). mouseClickListener:     whenever we press RMB out T figure changes it's location based on given coords.

--- ui/window.go
+++ ui/window.go
@@ -30,8 +30,9 @@ type Visualizer struct {
 	tx   chan screen.Texture
 	done chan struct{}
 
-	sz  size.Event
-	pos image.Rectangle
+	sz        size.Event
+	pos       image.Rectangle
+	tetCenter image.Point
 }
 
 func (pw *Visualizer) Main() {
@@ -39,6 +40,8 @@ func (pw *Visualizer) Main() {
 	pw.done = make(chan struct{})
 	pw.pos.Max.X = 200
 	pw.pos.Max.Y = 200
+	pw.tetCenter.X = WindowWidth / 2
+	pw.tetCenter.Y = WindowHeight / 2
 	driver.Main(pw.run)
 }
 
@@ -122,9 +125,13 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 	case mouse.Event:
 		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
-			if e.Button == mouse.ButtonRight {
-
+			// Реакція на натискання правої кнопки миші.
+			if e.Button == mouse.ButtonRight && e.Direction == mouse.DirPress {
+				pw.tetCenter = image.Point{
+					int(e.X),
+					int(e.Y),
+				}
+				pw.w.Send(paint.Event{})
 			}
 		}
 
@@ -143,7 +150,12 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.RGBA{0, 255, 0, 255}, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	// Відображення фігури Т синього кольору.
+	x, y := pw.tetCenter.X, pw.tetCenter.Y
+	c := color.RGBA{0, 0, 255, 1}
+
+	pw.w.Fill(image.Rect(x-150, y-100, x+150, y), c, draw.Src)
+	pw.w.Fill(image.Rect(x-50, y, x+50, y+100), c, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
