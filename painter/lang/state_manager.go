e90547dcf0289ba110b24eee8d42d1150b02c1fb
andrew <andry925@ukr.net>
2024-04-25 15:52:56 +0300

    fix:fix error in the state manager

--- painter/lang/state_manager.go
+++ painter/lang/state_manager.go
@@ -4,56 +4,53 @@ import (
 	"github.com/roman-mazur/architecture-lab-3/painter"
 )
 
-// Uistate preserves UI elements.
 type Uistate struct {
-	BackgroundColor painter.OperationFunc
+	backgroundColor painter.OperationFunc
 	Rect            painter.OperationFunc
 	Figures         []*painter.TFigure
 }
 
-// Instance of NewUistata.
 func NewUistate() *Uistate {
 	return &Uistate{}
 }
 
-// Sets background color.
-func (u *Uistate) SetBackgroundColor(op painter.OperationFunc) {
-	u.BackgroundColor = op
+func (f *Uistate) SetBgColor(op painter.OperationFunc) {
+	f.backgroundColor = op
 }
 
-// Draw operation.
-func (u *Uistate) SetRect(op painter.OperationFunc) {
-	u.Rect = op
+func (f *Uistate) SetRect(op painter.OperationFunc) {
+	f.Rect = op
 }
 
-// Reset all operations.
-func (u *Uistate) Reset() {
-	u.BackgroundColor = painter.OperationFunc(painter.BlackFill)
-	u.Rect = painter.Rectangle(0, 0, 0, 0)
-	u.Figures = nil
+func (f *Uistate) Reset() {
+	f.backgroundColor = painter.OperationFunc(painter.BlackFill)
+	f.Rect = painter.Rectangle(0, 0, 0, 0)
+	f.Figures = nil
 }
 
-// Update .
-func (u *Uistate) Update() []painter.Operation {
-	var operations []painter.Operation
+func (f *Uistate) Update() []painter.Operation {
+	var result []painter.Operation
 
-	if u.BackgroundColor != nil {
-		operations = append(operations, u.BackgroundColor)
+	if f.backgroundColor != nil {
+		result = append(result, f.backgroundColor)
 	}
-	if u.Rect != nil {
-		operations = append(operations, u.Rect)
-	}
-	for _, figure := range u.Figures {
-		operations = append(operations, figure.Drawfigure())
-	}
-	operations = append(operations, painter.UpdateOp)
 
-	return operations
+	if f.Rect != nil {
+		result = append(result, f.Rect)
+
+	}
+
+	for _, figure := range f.Figures {
+		result = append(result, figure.Drawfigure())
+	}
+
+	result = append(result, painter.UpdateOp)
+	return result
+
 }
 
-// MoveFigures moves all figures to specified coordinates.
-func (u *Uistate) MoveFigures(dx, dy int) {
-	for _, figure := range u.Figures {
+func (f *Uistate) MoveFigures(dx, dy int) {
+	for _, figure := range f.Figures {
 		figure.MoveFigure(dx, dy)
 	}
 }
